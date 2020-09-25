73ab354b10926ba32df3429bcf1f28e30db3af19
jeremyrice98 <ricefamily1003@gmail.com>
2020-09-25 12:00:22 -0600

    deploy to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jeremyrice98:jaxon2006@cluster0.cka8u.mongodb.net/deep_thoughts?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
